3264c0cf7fdd4973a615d061a6f626cd421b2998
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2022-07-14 13:41:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello! Closet Server demo! Demonstration - Thursday Demo\n');
+  res.send('Hello! Closet Server demo! Demonstration - Thursday Demoo\n');
 });
 
 var server = app.listen(3000, function () {
