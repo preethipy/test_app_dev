8c64fc7475874073b05831110a070519a8c6e65e
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2022-08-03 17:53:16 +0530

    demodemo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello! Closet Server demo! demo\n');
+  res.send('Hello! Closet Server demo! demo for HCL team 5:53\n');
 });
 
 var server = app.listen(3000, function () {
